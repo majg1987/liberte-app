af4e66f08b131cfe81cfee4c53fc8ed3fb6601ae
divRobles <j.roblesmendoza96@gmail.com>
2022-07-12 10:27:11 +0000

    finish-no-masonry

--- src/front/js/component/ProductCard.js
+++ src/front/js/component/ProductCard.js
@@ -1,19 +1,29 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import '../../styles/inicio.css'
+import '../../styles/productCard.css'
 
-export const ProductCard = ({ img }) => {
+export const ProductCard = ({ img, nombreArtista, precio }) => {
 
 
     return (
         <div className="grid col-lg-4 col-md-6 col-sm-12 col-productos">
+            {/* // <div className="grid col-12 col-productos"> */}
             <div className="grid-item container-producto">
-                <div className="info-artista">
+                <a href="" className="h-100 w-100">
+                    <div className="row info-artista">
+                        <div className="col-10 columna-nombre-artista">
+                            <p className="nombre-artista">
+                                {nombreArtista}
+                            </p>
+                        </div>
+                        <div className="col-2 columna-precio/obra">
+                            <p className="nombre-artista">
+                                {precio}
+                            </p>
+                        </div>
 
-                    <p className="nombre-artista">
-                        ihihi
-                    </p>
-                </div>
+                    </div>
+                </a>
                 <img src={img} alt="" className="img-producto" >
                 </img>
             </div>
--- src/front/js/pages/inicio.js
+++ src/front/js/pages/inicio.js
@@ -43,25 +43,37 @@ export const Inicio = () => {
             <div className="container container-lista-productos">
 
                 <div className="row  row-productos">
-                    {/* <div className="row grid row-productos" data-masonry='{"percentPosition": true}'> */}
+                    {/* <div className="row row-productos" data-masonry='{"percentPosition": true}'> */}
 
                     <ProductCard
                         img={"https://images.pexels.com/photos/12681200/pexels-photo-12681200.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"}
+                        nombreArtista='Pepe'
+                        precio='16€'
                     />
                     <ProductCard
                         img={"https://images.pexels.com/photos/11791794/pexels-photo-11791794.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"}
+                        nombreArtista='Juan'
+                        precio='1540€'
                     />
                     <ProductCard
-                        img={"https://images.pexels.com/photos/11791794/pexels-photo-11791794.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"}
+                        img={"https://images.pexels.com/photos/10433895/pexels-photo-10433895.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load"}
+                        nombreArtista='Rodolfo'
+                        precio='56€'
                     />
                     <ProductCard
-                        img={"https://images.pexels.com/photos/11791794/pexels-photo-11791794.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"}
+                        img={"https://images.pexels.com/photos/12499889/pexels-photo-12499889.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load"}
+                        nombreArtista='Obama'
+                        precio='15€'
                     />
                     <ProductCard
-                        img={"https://images.pexels.com/photos/11791794/pexels-photo-11791794.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"}
+                        img={"https://images.pexels.com/photos/6381090/pexels-photo-6381090.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load"}
+                        nombreArtista='Rosinni'
+                        precio='140€'
                     />
                     <ProductCard
-                        img={"https://images.pexels.com/photos/11791794/pexels-photo-11791794.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"}
+                        img={"https://images.pexels.com/photos/12641743/pexels-photo-12641743.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load"}
+                        nombreArtista='Paco'
+                        precio='400€'
                     />
 
 
