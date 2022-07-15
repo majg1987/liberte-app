9c129d8d4a61f2d946570fbe1f5d1d04764ac908
divRobles <j.roblesmendoza96@gmail.com>
2022-07-15 10:07:48 +0000

    routes-ok

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,80 +1,84 @@
-const getState = ({ getStore, getActions, setStore }) => {
-  return {
-    store: {
-      registro: false,
-      auth: false,
-      errorAuth: false,
-    },
-    actions: {
-      // Registro
-      registro: (nombre, apellidos, email, password, artista) => {
-        try {
-          // fetching data from the backend
-          fetch(process.env.BACKEND_URL + "/api/registration", {
-            method: "POST",
-            body: JSON.stringify({
-              nombre: nombre,
-              apellido: apellidos,
-              email: email,
-              password: password,
-              artista: artista,
-              dni: null,
-              nacimiento: null,
-              foto_usuario: null,
-              descripcion: null,
-            }),
-            headers: {
-              "Content-Type": "application/json",
+const getState = ({
+    getStore,
+    getActions,
+    setStore
+}) => {
+    return {
+        store: {
+            registro: false,
+            auth: false,
+            errorAuth: false,
+        },
+        actions: {
+            // Registro
+            registro: (nombre, apellidos, email, password, artista) => {
+                try {
+                    // fetching data from the backend
+                    fetch(process.env.BACKEND_URL + "/api/registration", {
+                        method: "POST",
+                        body: JSON.stringify({
+                            nombre: nombre,
+                            apellido: apellidos,
+                            email: email,
+                            password: password,
+                            artista: artista,
+                            dni: null,
+                            nacimiento: null,
+                            foto_usuario: null,
+                            descripcion: null,
+                        }),
+                        headers: {
+                            "Content-Type": "application/json",
+                        },
+                    }).then((response) => {
+                        if (response.status === 200) {
+                            setStore({
+                                registro: true,
+                            });
+                        }
+                        response.json();
+                        setStore({
+                            registro: false,
+                        });
+                    });
+                } catch (error) {
+                    console.log("Error loading message from backend", error);
+                }
             },
-          }).then((response) => {
-            if (response.status === 200) {
-              setStore({
-                registro: true,
-              });
-            }
-            response.json();
-            setStore({
-              registro: false,
-            });
-          });
-        } catch (error) {
-          console.log("Error loading message from backend", error);
-        }
-      },
 
-      // Fecth de Login
-      login: (email, password) => {
-        try {
-          // fetching data from the backend
-          fetch(process.env.BACKEND_URL + "/api/login", {
-            method: "POST",
-            body: JSON.stringify({
-              email: email,
-              password: password,
-            }),
-            headers: {
-              "Content-Type": "application/json",
+            // Fecth de Login
+            login: (email, password) => {
+                try {
+                    // fetching data from the backend
+                    fetch(process.env.BACKEND_URL + "/api/login", {
+                            method: "POST",
+                            body: JSON.stringify({
+                                email: email,
+                                password: password,
+                            }),
+                            headers: {
+                                "Content-Type": "application/json",
+                            },
+                        })
+                        .then((response) => {
+                            if (response.status === 200) {
+                                setStore({
+                                    auth: true,
+                                });
+                            } else {
+                                setStore({
+                                    errorAuth: true,
+                                });
+                            }
+                            response.json();
+                        })
+                        .then((data) => sessionStorage.setItem("token", data.access_token));
+                } catch (error) {
+                    console.log("Error loading message from backend", error);
+                }
             },
-          })
-            .then((response) => {
-              if (response.status === 200) {
-                setStore({
-                  auth: true,
-                });
-              } else {
-                setStore({
-                  errorAuth: true,
-                });
-              }
-              response.json();
-            })
-            .then((data) => sessionStorage.setItem("token", data.access_token));
-        } catch (error) {
-          console.log("Error loading message from backend", error);
-        }
-      },
-    },
-  };
+        },
+    };
 };
 
 export default getState;
