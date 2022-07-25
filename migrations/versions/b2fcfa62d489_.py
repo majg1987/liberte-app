13649f3593de43b63f7bd9a40208c9ad2ecc9923
majg1987 <majg1987@hotmail.com>
2022-07-25 14:43:45 +0000

    responsive login

--- migrations/versions/b2fcfa62d489_.py
+++ migrations/versions/b2fcfa62d489_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: fccd7b8f8d52
+Revision ID: b2fcfa62d489
 Revises: 
-Create Date: 2022-07-25 13:05:25.048367
+Create Date: 2022-07-25 14:35:36.051172
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'fccd7b8f8d52'
+revision = 'b2fcfa62d489'
 down_revision = None
 branch_labels = None
 depends_on = None
