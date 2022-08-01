e8acab64c69253a312f0e26f6dedeb65022abc71
javier-bustillo <jjavier.bustillo@gmail.com>
2022-08-01 18:04:56 +0000

    fix conflicts

--- src/api/utils.py
+++ src/api/utils.py
@@ -1,9 +1,5 @@
 from flask import jsonify, url_for
-<<<<<<< HEAD
-from datetime import datetime 
-=======
 from datetime import datetime
->>>>>>> 3ec60d1fc95b7a92a49955e8270376d38c1dac6a
 
 class APIException(Exception):
     status_code = 400
