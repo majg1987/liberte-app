2a7774d54b114e270e203a7115c61f648c2c3db0
majg1987 <majg1987@hotmail.com>
2022-12-05 19:25:42 +0000

    update

--- migrations/versions/929e8894d99b_.py
+++ migrations/versions/929e8894d99b_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 3f1ee8f98830
+Revision ID: 929e8894d99b
 Revises: 
-Create Date: 2022-09-28 12:33:20.855032
+Create Date: 2022-12-05 19:21:09.968190
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '3f1ee8f98830'
+revision = '929e8894d99b'
 down_revision = None
 branch_labels = None
 depends_on = None
